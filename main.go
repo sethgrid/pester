294b3a12d9546de3b8c05bde60c9d04a22dc7251
Seth Ammons <seth@sendgrid.com>
2015-05-23 08:56:34 -0700

    full client wrapping

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package pester
 
 import (
 	"fmt"
+	"io"
 	"math"
 	"net/http"
+	"net/url"
 	"sync"
 	"time"
 )
-
-type BackoffStrategy func(retry int) time.Duration
 
 type Client struct {
 	hc http.Client
@@ -22,9 +22,25 @@ type Client struct {
 	ErrLog []string
 }
 
+type result struct {
+	resp *http.Response
+	err  error
+}
+
+type params struct {
+	method   string
+	req      *http.Request
+	url      string
+	bodyType string
+	body     io.Reader
+	data     url.Values
+}
+
 func New() *Client {
 	return &Client{Concurrency: 1, MaxRetries: 3, Backoff: DefaultBackoff, ErrLog: []string{}}
 }
+
+type BackoffStrategy func(retry int) time.Duration
 
 func DefaultBackoff(_ int) time.Duration {
 	return 1 * time.Second
@@ -34,28 +50,37 @@ func ExponentialBackoff(i int) time.Duration {
 	return time.Duration(math.Pow(2, float64(i))) * time.Second
 }
 
-func (c *Client) Get(url string) (*http.Response, error) {
-	type result struct {
-		resp *http.Response
-		err  error
-	}
+func (c *Client) pester(p params) (*http.Response, error) {
 	resultCh := make(chan result)
 
 	for req := 0; req < c.Concurrency; req++ {
-		go func(n int) {
+		go func(n int, p params) {
 			resp := &http.Response{}
 			var err error
 
 			for i := 0; i < c.MaxRetries; i++ {
-				resp, err = c.hc.Get(url)
+
+				switch p.method {
+				case "Do":
+					resp, err = c.hc.Do(p.req)
+				case "Get":
+					resp, err = c.hc.Get(p.url)
+				case "Head":
+					resp, err = c.hc.Head(p.url)
+				case "Post":
+					resp, err = c.hc.Post(p.url, p.bodyType, p.body)
+				case "PostForm":
+					resp, err = c.hc.PostForm(p.url, p.data)
+				}
+
 				if err == nil && resp.StatusCode < 400 {
 					resultCh <- result{resp: resp, err: err}
 				}
-				c.log(fmt.Sprintf("GET %s [req %d::ret %d] :: %s", url, n, i, err.Error()))
+				c.log(fmt.Sprintf("GET %s :: [req %d :: ret %d] :: %s", p.url, n, i, err.Error()))
 				<-time.Tick(c.Backoff(i))
 			}
 			resultCh <- result{resp: resp, err: err}
-		}(req)
+		}(req, p)
 	}
 
 	for {
@@ -66,6 +91,26 @@ func (c *Client) Get(url string) (*http.Response, error) {
 	}
 
 	return nil, nil
+}
+
+func (c *Client) Do(req *http.Request) (resp *http.Response, err error) {
+	return c.pester(params{method: "Do", req: req})
+}
+
+func (c *Client) Get(url string) (resp *http.Response, err error) {
+	return c.pester(params{method: "Get", url: url})
+}
+
+func (c *Client) Head(url string) (resp *http.Response, err error) {
+	return c.pester(params{method: "Head", url: url})
+}
+
+func (c *Client) Post(url string, bodyType string, body io.Reader) (resp *http.Response, err error) {
+	return c.pester(params{method: "Post", url: url, bodyType: bodyType, body: body})
+}
+
+func (c *Client) PostForm(url string, data url.Values) (resp *http.Response, err error) {
+	return c.pester(params{method: "PostForm", url: url, data: data})
 }
 
 func (c *Client) log(msg string) {
